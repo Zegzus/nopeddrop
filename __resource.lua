9274ba53e21c219376fcd724de7901385f231fb6
Hexus <92677418+Zegzus@users.noreply.github.com>
2022-05-04 17:59:55 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+resource_manifest_version '66659leb-39dl-2456-d245-poe91kswioi9'
+
+author "!Hexus#2915"
+client_script "client/client.lua"
+description "Simple script to remove ammo drops from peds"
